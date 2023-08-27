316123c2810f1545945e0eba69435e0b6092aa8d
srinath-3898 <srinath3898@gmail.com>
2023-08-27 17:36:09 +0530

    signin completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ const signup = async (req, res) => {
     const userExits = await User.findOne({ where: { email } });
     if (userExits) {
       return res
-        .status(400)
+        .status(403)
         .json({ status: false, data: null, message: "Account already exists" });
     }
     const user = await User.create({ fullName, email, mobile, password });
@@ -32,4 +32,30 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        status: false,
+        data: null,
+        message: "Email or password missing",
+      });
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: false, data: null, message: "User not found" });
+    }
+    return res
+      .status(201)
+      .json({ status: true, data: user, message: "Logged in successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, data: null, message: error.message });
+  }
+};
+
+module.exports = { signup, signin };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signup } = require("../controllers/userController");
+const { signup, signin } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
